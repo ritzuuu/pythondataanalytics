cd18fac904c6aca220d3bd3298ce6bde4059577e
Anup <163889469+ritzuuu@users.noreply.github.com>
2024-05-14 11:07:45 +0530

    Add files via upload

---
+++ Untitled7.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+city=input("enter city name")
+if city=="davangere":
+    print("you loves benne dosa")
+elif city=="bengaluru":  
+    print("you loves pub and party")
+elif city=="haveri":   
+    print("you loves egg rice")
+else :
+    print("none of your Buisness")
+
